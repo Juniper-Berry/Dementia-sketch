1c4f72967442f30ca84de54e3ee2f2f155b14989
Juniper-Berry <alwi1816@colorado.edu>
2020-09-03 17:29:44 -0600

    Starting the project, next time just make it once you have the repo

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let myFont;
+function preload() {
+  myFont = loadFont('assets/inconsolata.otf');
+}
+
+function setup() {
+  fill('#ED225D');
+  textFont(myFont);
+  textSize(36);
+  text('p5*js', 10, 50);
+}
+
+function draw() {
+
+  // put drawing code here
+}
